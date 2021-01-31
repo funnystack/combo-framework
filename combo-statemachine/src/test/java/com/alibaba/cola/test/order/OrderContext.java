5f9d7092d533c809f8c21303229e0f353d3d4068
fangli <7129at.com8>
2021-01-31 22:27:53 +0800

    订单转换demo

---
+++ combo-statemachine/src/test/java/com/alibaba/cola/test/order/OrderContext.java
@@ -0,0 +1,13 @@
+package com.alibaba.cola.test.order;
+
+public class OrderContext {
+    private String orderId;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+}
